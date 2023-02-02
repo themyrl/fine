368f52336e1ff043dc76a837fe6c0f7ae0b98648
themyrl <loic.themyr@lecnam.net>
2023-02-02 17:05:56 +0100

    livus nnunet training

--- fine_package/fine/run/run_all_unet.py
+++ fine_package/fine/run/run_all_unet.py
@@ -3,23 +3,13 @@ import run_training as run
 
 g = '0'
 
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNET', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNET', val=True, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='NNUNETaf', val=False, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='NNUNETaf', val=True, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=2, outpath='NNUNETaf', val=False, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=2, outpath='NNUNETaf', val=True, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=3, outpath='NNUNETaf', val=False, npz=True, na=True)
-# # run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=3, outpath='NNUNETaf', val=True, npz=True, na=True)
+
+# BCV
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNET', val=False, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='NNUNET', val=True, npz=True, na=True)
 
 
+# Livus
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='130', fold=0, outpath='NNUNET', val=False, npz=True, na=True)
+run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='130', fold=0, outpath='NNUNET', val=True, npz=True, na=True)
 
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=2, outpath='nnffNNUNET', val=False, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=2, outpath='nnffNNUNET', val=True, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=3, outpath='nnffNNUNET', val=False, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=3, outpath='nnffNNUNET', val=True, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='nnffNNUNET', val=False, npz=True, na=True)
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=1, outpath='nnffNNUNET', val=True, npz=True, na=True)
-
-# g='1'
-# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2', task='017', fold=0, outpath='nnffNNUNET', val=False, npz=True, na=True,dbg=True)

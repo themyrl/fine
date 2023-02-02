5e61c91eab59383976674b3512997251024b8db9
themyrl <loic.themyr@lecnam.net>
2023-02-02 17:07:25 +0100

    livus nnunet training

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=expplanning     # job name
+#SBATCH --job-name=unet_livus     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:30:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-dev
-#SBATCH --output=logs/expplanning.out # output file name # add %j to id the job
-#SBATCH --error=logs/expplanning.err  # error file name # add %j to id the job
+#SBATCH --output=logs/unet_livus.out # output file name # add %j to id the job
+#SBATCH --error=logs/unet_livus.err  # error file name # add %j to id the job
 # # SBATCH -C v100-32g
 
 
@@ -31,12 +31,12 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 # convert dataset
 # srun python nnUNet/nnunet/dataset_conversion/Task017_BeyondCranialVaultAbdominalOrganSegmentation.py
-srun python nnUNet/nnunet/dataset_conversion/Task130_Livus.py
+# srun python nnUNet/nnunet/dataset_conversion/Task130_Livus.py
 
 # planning and pre-processing
 # srun python nnUNet/nnunet/experiment_planning/nnUNet_plan_and_preprocess.py -t 017 --verify_dataset_integrity
-srun python nnUNet/nnunet/experiment_planning/nnUNet_plan_and_preprocess.py -t 130 --verify_dataset_integrity
+# srun python nnUNet/nnunet/experiment_planning/nnUNet_plan_and_preprocess.py -t 130 --verify_dataset_integrity
 
 
 # training
-# srun python fine_package/fine/run/run_all_unet.py
+srun python fine_package/fine/run/run_all_unet.py

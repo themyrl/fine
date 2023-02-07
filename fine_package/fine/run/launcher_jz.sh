2cbf081e9e5a900ba5dd06a7373c0b36942105d7
themyrl <loic.themyr@lecnam.net>
2023-02-07 17:37:23 +0100

    word nnformer

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=cotr_word     # job name
+#SBATCH --job-name=nnfo_word     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=99:30:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/cotr_word.out # output file name # add %j to id the job
-#SBATCH --error=logs/cotr_word.err  # error file name # add %j to id the job
+#SBATCH --output=logs/nnfo_word.out # output file name # add %j to id the job
+#SBATCH --error=logs/nnfo_word.err  # error file name # add %j to id the job
 # # SBATCH -C v100-32g
 
 
@@ -42,6 +42,6 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 # training
 # srun python fine_package/fine/run/run_all_unet.py #unet_livus & unet_livus_bis & unet_word
-srun python fine_package/fine/run/run_all_cotr.py #cotr_livus & cotr_word
-# srun python fine_package/fine/run/run_all_nnformer.py #nnformer_livus
+# srun python fine_package/fine/run/run_all_cotr.py #cotr_livus & cotr_word 
+srun python fine_package/fine/run/run_all_nnformer.py #nnformer_livus & nnfo_word
 # srun python fine_package/fine/run/run_all_fine.py #fine_livus

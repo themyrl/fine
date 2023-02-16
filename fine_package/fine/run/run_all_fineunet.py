f3a080382dd09d7fc8a369fef861e034c975b6a3
themyrl <loic.themyr@lecnam.net>
2023-02-16 11:12:28 +0100

    fineunet livus

--- fine_package/fine/run/run_all_fineunet.py
+++ fine_package/fine/run/run_all_fineunet.py
@@ -1,5 +1,7 @@
 # import UTrans.run.nnunet_run_training as run
 import run_training as run
+import argparse
+
 
 g = '0'
 
@@ -16,6 +18,18 @@ g = '0'
 
 
 # WORD
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_fineUNet', task='140', fold=0, outpath='FINENNUNET', val=False, npz=True, na=True)
-run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_fineUNet', task='140', fold=0, outpath='FINENNUNET', val=True, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_fineUNet', task='140', fold=0, outpath='FINENNUNET', val=False, npz=True, na=True)
+# run.main(gpu=g, network='3d_fullres', network_trainer='nnUNetTrainerV2_fineUNet', task='140', fold=0, outpath='FINENNUNET', val=True, npz=True, na=True)
 
+# ALL
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-n", "--network_trainer")
+	parser.add_argument("-t", "--task", type=str)
+	parser.add_argument("-o", "--outpath")
+	parser.add_argument("-a", "--na", type=bool)
+
+	args = parser.parse_args()
+
+	run.main(gpu=g, network='3d_fullres', network_trainer=args['network_trainer'], task=args['task'], fold=0, outpath=args['outpath'], val=False, npz=True, na=args['na'])
+	run.main(gpu=g, network='3d_fullres', network_trainer=args['network_trainer'], task=args['task'], fold=0, outpath=args['outpath'], val=True, npz=True, na=args['na'])

52cda29737dad8066a578f1340e1e1c41fae764b
themyrl <loic.themyr@lecnam.net>
2023-02-20 15:38:42 +0100

    nnunet word eval

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=finun_word_eval     # job name
+#SBATCH --job-name=unet_word_128_eval     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=99:59:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/finun_word_eval.out # output file name # add %j to id the job
-#SBATCH --error=logs/finun_word_eval.err  # error file name # add %j to id the job
+#SBATCH --output=logs/unet_word_128_eval.out # output file name # add %j to id the job
+#SBATCH --error=logs/unet_word_128_eval.err  # error file name # add %j to id the job
 # # SBATCH -C v100-32g
  
 
@@ -52,4 +52,5 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 
 ## Only eval
-srun python fine_package/fine/run/run_all_fineunet.py nnUNetTrainerV2_fineUNet 140 FINENNUNET 1 1 #finun_word_eval
+# srun python fine_package/fine/run/run_all_fineunet.py nnUNetTrainerV2_fineUNet 140 FINENNUNET 1 1 #finun_word_eval
+srun python fine_package/fine/run/run_all_fineunet.py nnUNetTrainerV2 140 NNUNET_128_128_64 1 1 #unet_word_128_eval
---
+++ fine_package/fine/run/run.py
@@ -0,0 +1,18 @@
+import run_training as run
+import sys
+
+
+g = '0'
+# ALL
+if __name__ == '__main__':
+	network_trainer = sys.argv[1]
+	task = str(sys.argv[2])
+	outpath = sys.argv[3]
+	na = bool(int(sys.argv[4]))
+	ov = bool(int(sys.argv[5]))
+
+	if not ov:
+		run.main(gpu=g, network='3d_fullres', network_trainer=network_trainer, task=task, fold=0, outpath=outpath, val=False, npz=True, na=na)
+		run.main(gpu=g, network='3d_fullres', network_trainer=network_trainer, task=task, fold=0, outpath=outpath, val=True, npz=True, na=na)
+	else:
+		run.main(gpu=g, network='3d_fullres', network_trainer=network_trainer, task=task, fold=0, outpath=outpath, val=True, npz=True, na=na)

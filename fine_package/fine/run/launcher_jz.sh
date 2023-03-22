34a88221a5c330b355130ce65be4060e56810a6a
themyrl <loic.themyr@lecnam.net>
2023-03-22 11:12:33 +0100

    word finev3+nnUNet V2 - training

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=20   #10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=00:10:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-dev
+#SBATCH --time=99:10:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --output=logs/funv3v2_word.out # output file name # add %j to id the job
 #SBATCH --error=logs/funv3v2_word.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g

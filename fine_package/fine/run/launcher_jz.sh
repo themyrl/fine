0ad327d883290736e40be922e8c19eaae96bb75b
themyrl <loic.themyr@lecnam.net>
2023-05-02 11:07:34 +0200

    bcv nnformer fine notta - training c

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=cofi_bcv_nta     # job name
+#SBATCH --job-name=finv32_bcv_nta_c     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=20   #10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
-#SBATCH --time=99:15:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/cofi_bcv_nta.out # output file name # add %j to id the job
-#SBATCH --error=logs/cofi_bcv_nta.err  # error file name # add %j to id the job
+#SBATCH --time=19:15:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --output=logs/finv32_bcv_nta_c.out # output file name # add %j to id the job
+#SBATCH --error=logs/finv32_bcv_nta_c.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
  
 
@@ -35,10 +35,10 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_nnFormer -task 017 -outpath notta_NNFORMER #nnf_bcv_nta
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glam -task 017 -outpath notta_GLAMV2 #glamv2_bcv_nta
-# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 017 -outpath notta_FINEV32 #finv32_bcv_nta
+srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 017 -outpath notta_FINEV32 -continu #finv32_bcv_nta ; finv32_bcv_nta_c
 
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 017 -outpath notta_COTR -na # cotr_bcv_nta
-srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 017 -outpath notta_COTRFINE -na # cofi_bcv_nta
+# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 017 -outpath notta_COTRFINE -na # cofi_bcv_nta
 
 
 ## convert dataset

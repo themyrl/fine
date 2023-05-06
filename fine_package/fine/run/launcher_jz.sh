37a14d8ff98b095f8dbbd30240934e9ec6bf360e
themyrl <loic.themyr@lecnam.net>
2023-05-06 13:06:02 +0200

    bcv nnunet fine notta folddef - training

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=glv2un_bcv_nta_f0     # job name
+#SBATCH --job-name=funv32_bcv_nta_f0     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=99:15:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/glv2un_bcv_nta_f0.out # output file name # add %j to id the job
-#SBATCH --error=logs/glv2un_bcv_nta_f0.err  # error file name # add %j to id the job
+#SBATCH --output=logs/funv32_bcv_nta_f0.out # output file name # add %j to id the job
+#SBATCH --error=logs/funv32_bcv_nta_f0.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
  
 
@@ -30,8 +30,8 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 ### BCV
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2 -task 017 -outpath f0notta_NNUNET -na #nnun_bcv_nta ; nnun_bcv_nta_f0 
-srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glamUNet_v2 -task 017 -outpath f0notta_GLAMV2NNUNET -na #glv2un_bcv_nta ; glv2un_bcv_nta_f0
-# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3UNet_v2 -task 017 -outpath f0notta_FINEV32NNUNETV2 -na #funv32_bcv_nta ; funv32_bcv_nta_f0
+# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glamUNet_v2 -task 017 -outpath f0notta_GLAMV2NNUNET -na #glv2un_bcv_nta ; glv2un_bcv_nta_f0
+srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3UNet_v2 -task 017 -outpath f0notta_FINEV32NNUNETV2 -na #funv32_bcv_nta ; funv32_bcv_nta_f0
 
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_nnFormer -task 017 -outpath f0notta_NNFORMER #nnf_bcv_nta ; nnf_bcv_nta_f0
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glam -task 017 -outpath f0notta_GLAMV2 #glamv2_bcv_nta ; glamv2_bcv_nta_f0

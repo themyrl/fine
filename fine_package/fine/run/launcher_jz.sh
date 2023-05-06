3bf99fdb280e141bc10c286a6279a0ec7e155b72
themyrl <loic.themyr@lecnam.net>
2023-05-06 13:08:02 +0200

    bcv cotr notta folddef - training

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=finv32_bcv_nta_f0     # job name
+#SBATCH --job-name=cotr_bcv_nta_f0     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=99:15:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/finv32_bcv_nta_f0.out # output file name # add %j to id the job
-#SBATCH --error=logs/finv32_bcv_nta_f0.err  # error file name # add %j to id the job
+#SBATCH --output=logs/cotr_bcv_nta_f0.out # output file name # add %j to id the job
+#SBATCH --error=logs/cotr_bcv_nta_f0.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
  
 
@@ -35,9 +35,9 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_nnFormer -task 017 -outpath f0notta_NNFORMER #nnf_bcv_nta ; nnf_bcv_nta_f0
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glam -task 017 -outpath f0notta_GLAMV2 #glamv2_bcv_nta ; glamv2_bcv_nta_f0
-srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 017 -outpath f0notta_FINEV32 -continu #finv32_bcv_nta ; finv32_bcv_nta_c ; finv32_bcv_nta_f0
+# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 017 -outpath f0notta_FINEV32 -continu #finv32_bcv_nta ; finv32_bcv_nta_c ; finv32_bcv_nta_f0
 
-# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 017 -outpath f0notta_COTR -na # cotr_bcv_nta ; cotr_bcv_nta_f0
+srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 017 -outpath f0notta_COTR -na # cotr_bcv_nta ; cotr_bcv_nta_f0
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 017 -outpath f0notta_COTRFINE -na # cofi_bcv_nta ; cofi_bcv_nta_f0
 
 

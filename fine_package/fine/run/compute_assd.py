5c4fe8c0cafa5a498f00e48bc3aea9e6fa5b8a03
themyrl <loic.themyr@lecnam.net>
2023-06-01 22:15:15 +0200

    assd - dbg

--- fine_package/fine/run/compute_assd.py
+++ fine_package/fine/run/compute_assd.py
@@ -26,70 +26,82 @@ if __name__ == '__main__':
 
 	args = parser.parse_args()
 
-
-	path = args.path
 	n_classe = args.n_classe
-	network = "{}_IN_LeakyReLU/".format(args.network)
-	task = "Task{}_{}/".format(args.task, all_tasks[args.task])
+	path = args.path
+	all_tasks = os.lisdir(path)
+	for task in all_tasks:
+		all_network = os.lisdir(os.join(path, task))
+		for network in all_network:
+			pps = ["fold_0/validation_raw", "fold_0/validation_raw_postprocessed"]
+			p = 0
+			for pp in pps:
+				try:
+					# network = "{}_IN_LeakyReLU/".format(args.network)
+					# task = "Task{}_{}/".format(args.task, all_tasks[args.task])
 
-	pred = "fold_0/validation_raw"
-	if args.pp:
-		pred += "_postprocessed"
-	pred += "/"
+					# pred = "fold_0/validation_raw"
+					# if args.pp:
+						# pred += "_postprocessed"
+					# pred += "/"
 
 
-	target_fold = os.path.join(path, task, network, "gt_niftis/")
-	pred_fold = os.path.join(path, task, network, pred)
-	outpath = os.path.join(path, task, network, "assd_results.json")
+					target_fold = os.path.join(path, task, network, "gt_niftis/")
+					pred_fold = os.path.join(path, task, network, pp)
+					outpath = os.path.join(path, task, network, "assd_results_{}.json".format(p))
+
+					print("------------------------------------------------------------------------------")
+					print(pred_fold)
 
 
-	results = {}
-	res_per_classes_ = {}
-	for i in range(1, n_classe+1):
-		res_per_classes_[str(i)] = []
+					results = {}
+					res_per_classes_ = {}
+					for i in range(1, n_classe+1):
+						res_per_classes_[str(i)] = []
 
-	# compute assd per patient
-	for i in os.listdir(pred_fold):
-		if "nii.gz" in i:
-			patient_id = i.split(".")[0]
-			print(patient_id)
+					# compute assd per patient
+					for i in os.listdir(pred_fold):
+						if "nii.gz" in i:
+							patient_id = i.split(".")[0]
+							print(patient_id)
 
-			res = {}
+							res = {}
 
-			target = nib.load(os.path.join(target_fold, i)).get_fdata()
-			pred = nib.load(os.path.join(pred_fold, i)).get_fdata()
+							target = nib.load(os.path.join(target_fold, i)).get_fdata()
+							pred = nib.load(os.path.join(pred_fold, i)).get_fdata()
 
-			print()
-			print(np.unique(target))
-			print(np.unique(pred))
+							# print()
+							# print(np.unique(target))
+							# print(np.unique(pred))
 
-			for j in range(1, n_classe+1):
-				res[str(j)] = assd(1*(pred == j), 1*(target == j))
-				res_per_classes_[str(j)].append(res[str(j)])
-				print(res[str(j)], end=" ")
+							for j in range(1, n_classe+1):
+								res[str(j)] = assd(1*(pred == j), 1*(target == j))
+								res_per_classes_[str(j)].append(res[str(j)])
+								print(res[str(j)], end=" ")
 
-			results[patient_id] = res
-			print("\n\n")
+							results[patient_id] = res
+							print("\n\n")
 
-	# compute mean and std
-	alls = {}
-	means = []
-	stds = []
-	for i in range(1, n_classe+1):
-		alls[str(i)] = {"mean": np.mean(res_per_classes_[str(i)]),
-						"std": np.std(res_per_classes_[str(i)])}
-		means.append(alls[str(i)]["mean"])
-		stds.append(alls[str(i)]["std"])
+					# compute mean and std
+					alls = {}
+					means = []
+					stds = []
+					for i in range(1, n_classe+1):
+						alls[str(i)] = {"mean": np.mean(res_per_classes_[str(i)]),
+										"std": np.std(res_per_classes_[str(i)])}
+						means.append(alls[str(i)]["mean"])
+						stds.append(alls[str(i)]["std"])
 
-	alls["mean_all"] = {"mean": np.mean(means),
-						"std": np.mean(stds)}
+					alls["mean_all"] = {"mean": np.mean(means),
+										"std": np.mean(stds)}
 
-	print(alls)
+					print(alls)
 
-	results["all"] = alls
+					results["all"] = alls
 
-	with open(outpath, 'w', encoding='utf-8') as f:
-		json.dump(results, f, ensure_ascii=False, indent=4)
+					with open(outpath, 'w', encoding='utf-8') as f:
+						json.dump(results, f, ensure_ascii=False, indent=4)
+				except:
+					pass
 
 
 

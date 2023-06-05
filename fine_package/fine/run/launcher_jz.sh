f425729573adbfa479c256aca7441b97b0b7fb02
themyrl <loic.themyr@lecnam.net>
2023-06-05 15:27:54 +0200

    assd

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=finv5_bcv_nta_f0     # job name
+#SBATCH --job-name=assd_2     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=99:15:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/finv5_bcv_nta_f0.out # output file name # add %j to id the job
-#SBATCH --error=logs/finv5_bcv_nta_f0.err  # error file name # add %j to id the job
+#SBATCH --output=logs/assd_2.out # output file name # add %j to id the job
+#SBATCH --error=logs/assd_2.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
  
 
@@ -28,7 +28,7 @@ export nnUNet_preprocessed="/gpfsscratch/rech/arf/unm89rb/nnUNet_preprocessed"
 export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 
 
-# srun python fine_package/fine/run/compute_assd.py -network notta_FINEV32NNUNETV2 -task 130 -n_classe 4
+srun python fine_package/fine/run/compute_assd.py -network notta_FINEV32NNUNETV2 -task 130 -n_classe 4 #assd, assd_2
 
 ### BCV
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2 -task 017 -outpath f0notta_NNUNET -na #nnun_bcv_nta ; nnun_bcv_nta_f0 
@@ -40,7 +40,7 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glam -task 017 -outpath f0notta_GLAMV2 #glamv2_bcv_nta ; glamv2_bcv_nta_f0
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 017 -outpath f0notta_FINEV32 -continu #-continu #finv32_bcv_nta ; finv32_bcv_nta_c ; finv32_bcv_nta_f0 ; finv32_bcv_nta_f0_c
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev4 -task 017 -outpath f0notta_FINEV4 -continu # finv4_bcv_nta_f0 ; finv4_bcv_nta_f0_c
-srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev5 -task 017 -outpath f0notta_FINEV5 #-continu # finv5_bcv_nta_f0 ; finv5_bcv_nta_f0_c
+# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev5 -task 017 -outpath f0notta_FINEV5 #-continu # finv5_bcv_nta_f0 ; finv5_bcv_nta_f0_c
 
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 017 -outpath f0notta_COTR -na # cotr_bcv_nta ; cotr_bcv_nta_f0
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 017 -outpath f0notta_COTRFINE -na # cofi_bcv_nta ; cofi_bcv_nta_f0

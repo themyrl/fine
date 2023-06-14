5b1db349937428d66e2dd86b36300298f169f70b
themyrl <loic.themyr@lecnam.net>
2023-06-14 14:32:39 +0200

    word ablation ntok - debug

--- fine_package/fine/network_architecture/finev3.py
+++ fine_package/fine/network_architecture/finev3.py
@@ -443,7 +443,9 @@ class SwinTransformerBlock(nn.Module):
         vts = vts + self.drop_path(self.mlp2(self.norm4(vts)))              #    !!!!!!!! on a modifié ici !!!!!!!!!!
         
         if self.vt_num != 1:
+            print(vts.shape)
             vts = rearrange(vts, "b (g n) c -> b g (n c)", n=self.vt_num)
+            print(vts.shape)
                
 
         # merge windows

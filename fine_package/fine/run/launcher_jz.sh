ce5944400aabf3cf1bf2e3cb1fa2f62a2d086516
themyrl <loic.themyr@lecnam.net>
2023-06-21 11:36:44 +0200

    debug

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=debug_7     # job name
+#SBATCH --job-name=debug_8     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -7,8 +7,8 @@
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --time=00:10:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-dev
-#SBATCH --output=logs/debug_7.out # output file name # add %j to id the job
-#SBATCH --error=logs/debug_7.err  # error file name # add %j to id the job
+#SBATCH --output=logs/debug_8.out # output file name # add %j to id the job
+#SBATCH --error=logs/debug_8.err  # error file name # add %j to id the job
 #SBATCH -C v100-32g
  
 
@@ -40,8 +40,8 @@ export RESULTS_FOLDER="/gpfsscratch/rech/arf/unm89rb/nnUNet_trained_models"
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_glam -task 140 -outpath DEBUG #debug_5
 # srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_finev3 -task 140 -outpath DEBUG #debug_6
 
-srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 140 -outpath DEBUG -na #debug_7
-# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 140 -outpath DEBUG -na #debug_8
+# srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_agno -task 140 -outpath DEBUG -na #debug_7
+srun python fine_package/fine/run/run.py -network nnUNetTrainerV2_CoTR_FINE -task 140 -outpath DEBUG -na #debug_8
 
 
 

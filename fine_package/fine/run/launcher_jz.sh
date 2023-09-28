2297f7b43323e9f85d72b159f143d6ef8296f508
themyrl <loic.themyr@lecnam.net>
2023-09-28 15:03:00 +0200

    fine these end

--- fine_package/fine/run/launcher_jz.sh
+++ fine_package/fine/run/launcher_jz.sh
@@ -9,7 +9,7 @@
 #SBATCH --qos=qos_gpu-dev
 #SBATCH --output=logs/fbv1.out # output file name # add %j to id the job
 #SBATCH --error=logs/fbv1.err  # error file name # add %j to id the job
-#SBATCH -C v100-32g
+#SBATCH -C a100
  
 
 cd $WORK/fine
